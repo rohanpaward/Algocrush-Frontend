e69e551c14237b19360411d19660d75c17943296
Rohan Powar <rohan.powar@ideaforgetech.com>
2026-04-18 18:40:23 +0530

    Fixed the url for deployed version to test

--- src/services/landing/landing-pages-services.js
+++ src/services/landing/landing-pages-services.js
@@ -1,6 +1,7 @@
 const loginWithGoogle = () => {
     window.location.href =
-      "http://localhost:3001/api/v1/algocrush/auth/google";
+      // "http://localhost:3001/api/v1/algocrush/auth/google";
+      "https://algocrush-frontend.vercel.app/api/v1/algocrush/auth/google"
   };
 
 export default loginWithGoogle;
