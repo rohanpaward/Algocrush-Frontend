7c22141d3aaebe4b775c1cab6ad6bc6667202258
Rohan Powar <rohan.powar@ideaforgetech.com>
2026-04-18 19:56:15 +0530

    Added logs

--- src/pages/auth/OAuthSuccess.jsx
+++ src/pages/auth/OAuthSuccess.jsx
@@ -12,12 +12,16 @@ const OAuthSuccess = () => {
 
     const init = async () => {
       try {
+        console.log("FULL URL:", window.location.href); // ✅ add this
+        console.log("SEARCH:", window.location.search); // ✅ add this
+      
         // Get token from URL
         const params = new URLSearchParams(window.location.search);
         const token = params.get("token");
 
         if (token) {
           localStorage.setItem("token", token);
+          console.log(token,'this is token')
         }
 
         const data = await getMe();
