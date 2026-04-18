4668f592680e3ea99c460111cc8fce83bdae1efb
Rohan Powar <rohan.powar@ideaforgetech.com>
2026-04-18 19:06:45 +0530

    Fixed the depolyed version url

--- src/services/landing/landing-pages-services.js
+++ src/services/landing/landing-pages-services.js
@@ -1,7 +1,7 @@
 const loginWithGoogle = () => {
     window.location.href =
       // "http://localhost:3001/api/v1/algocrush/auth/google";
-      "https://algocrush-node.onrender.com/api/v1/algocrush/auth/google/callback"
+      "https://algocrush-node.onrender.com/api/v1/algocrush/auth/google/"
   };
 
 export default loginWithGoogle;
